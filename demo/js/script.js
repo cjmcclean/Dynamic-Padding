5f0b1bae07ec33df7d721685a5e36234e76975ad
Chris McClean <cmcclean@empyreanbenefits.com>
2013-09-10 15:39:19 -0500

    dynPadding v2.0 Update
    - Update plugin to include methods (init, destroy) to add more
    consistent functionality in RWD.
    - Add in variable to control upper limit of padding.

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,18 +1,39 @@
 /* Author: Chris McClean */
 
+// call parent, and create settings array
+var navObj = $('#nav'),
+    options = {
+        targets: '#nav a',                  // the elements you want dynamically padded
+        remainder: '#nav .remainder',       // element for overflow padding to run off into
+        vertical: false,                    // if you want to dynamically pad a vertical list,
+        paddingBoundary: 34                 // set an upper bound on the padding that is applied per element
+    };
 
-// Call to dynamicPadding function
-$('#nav').dynPadding({
-	targets: '#nav a', // the elements you want dynamically padded
-	remainder: '#nav .remainder', // element for overflow padding to run off into
-	vertical: false // if you want to dynamically pad a vertical list
+if ($(window).width() >= 768) {
+    // Call to dynamicPadding function
+    $(navObj).dynPadding('init', options);
+}
+
+$(window).resize(function () {
+    var windowWidth = $(window).width();
+
+    // if our window width is below 767px remove dynPadding functionality
+    if (windowWidth < 767) {
+        // Call to remove dynamicPadding function
+        $(navObj).dynPadding('destroy', options);
+    }
+    // if our width is above or equal to 768px AND padding hasn't already been applied
+    else if (windowWidth >= 768 && $(navObj).has('.dynPadding_parent') != true) {
+        // Call to dynamicPadding function
+        $(navObj).dynPadding('init', options);
+    }
 });
 
 
 
 // usage: log('inside coolFunc',this,arguments);  |  paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function() {
-	log.history = log.history || [];   // store logs to an array for reference
+	log.history = log.history || [];        // store logs to an array for reference
 	log.history.push(arguments);
  	if (this.console) {
     		console.log( Array.prototype.slice.call(arguments) );
--- jq.dynamicPadding.js
+++ jq.dynamicPadding.js
@@ -1,99 +1,201 @@
 /**
  * Dynamic Padding
- * @ver: 1.2
+ * @ver: 2.0
  * @author: Chris McClean
  * 
- * Copyright 2013, Chris McClean
+ * Copyright September 2013, Chris McClean
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://www.gnu.org/licenses/gpl.html  |  http://opensource.org/licenses/mit-license.php
  * 
  * Credits: Original script written by Les James @ http://www.lesjames.com
  **/
-(function($) {
+(function ($) {
+
+    // Create an object literal for our methods
+    var methods = {
+        // Define individual methods within the literal
+        init: function (opts) {
+
+            // Repeat over each element in selector, taken from our main function and moved into each method for flexibility
+            return this.each(function () {
+
+                // Create a jQuery object to use with this individual element
+                var parent = $(this);
+
+                // Attempt to grab saved settings, if they don't exist we'll get "undefined".
+                var settings = $(parent).data('dynPadding');
+
+                // If we could't grab settings, create them from defaults and passed options
+                if (typeof (settings) == 'undefined') {
+
+                    var defaults = {
+                        targets: '#nav a',
+                        remainder: null,
+                        vertical: false,
+                        paddingBoundary: 34
+                    };
+
+                    settings = $.extend(defaults, opts);
+
+                    // Save our newly created settings
+                    $(parent).data('dynPadding', settings);
+
+                } else {
+
+                    // We got settings, merge our passed options in with them (optional)
+                    settings = $.extend({}, settings, opts);
+
+                    // If you wish to save options passed each time, add:
+                    // $(parent).data('dynPadding', settings);
+
+                }
+
+                var parent_size = 0,
+                    link_size   = 0,
+                    unpadded    = 0,
+                    link_count  = $(opts.targets).length;
+
+                if (opts.vertical) {
+                    parent_size = parent.height();
+                } else {
+                    parent_size = parent.width();
+                }
+
+                $(parent).addClass('dynPadding_parent');
+
+                // find unpadded size of links and reset link size to whole numbers
+                if (opts.vertical) {
+
+                    $(opts.targets).each(function () {
+                        // unpad element
+                        $(this).css({ 'padding-top': '0', 'padding-bottom': '0' });
+
+                        // measure element size with borders
+                        unpadded += $(this).outerHeight();
+
+                        // set element size to erase decimal sizing
+                        link_size = $(this).height();
+                        $(this).css({ height: link_size + 'px' });
+                    });
+
+                } else {
+
+                    $(opts.targets).each(function () {
+                        $(this).css({ 'padding-left': '0', 'padding-right': '0' });
+                        unpadded += $(this).outerWidth(true);
+                        link_size = $(this).width() + 1;
+                        $(this).css({ width: link_size + 'px' });
+                    });
+
+                }
+
+                // find raw padding and convert to whole number padding
+                var raw_padding = (parent_size - unpadded) / link_count,
+                    padding     = Math.floor(raw_padding);
+
+                // DEFAULT UPPER PADDING BOUNDARY: set an upper bound on the amount of padding
+                if (padding >= opts.paddingBoundary) { padding = parseInt(opts.paddingBoundary - 2); }
+
+                // if we have remainder enabled...
+                if (opts.remainder != null) {
+                    
+                    // calculate remainder from decimal padding
+                    remainder = Math.round((raw_padding - padding) * link_count);
+
+                    // DEFAULT UPPER REMAINDER BOUNDARY: set an upper bound on the amount of padding
+                    if (remainder >= Math.floor(opts.paddingBoundary / 2)) { remainder = (Math.floor(opts.paddingBoundary / 2) - 1); }
+
+                    if (opts.vertical) {
+                        // apply padding to links (if padding is a decimal apply floor to left and ceil to right)
+                        $(opts.targets).css({
+                            'padding-top': Math.floor(padding / 2), 'padding-bottom': Math.floor(padding / 2)
+                        });
+                        // apply remainder
+                        $(opts.remainder).css({
+                            'padding-top': Math.floor(remainder / 2) + Math.floor(padding / 2), 'padding-bottom': Math.floor(remainder / 2) + Math.floor(padding / 2)
+                        });
+                    } else {
+                        $(opts.targets).css({
+                            'padding-left': Math.floor(padding / 2), 'padding-right': Math.floor(padding / 2)
+                        });
+                        $(opts.remainder).css({
+                            'padding-left': Math.floor(remainder / 2) + Math.floor(padding / 2), 'padding-right': Math.floor(remainder / 2) + Math.floor(padding / 2)
+                        });
+                    }
+
+                } else {
+                    
+                    if (opts.vertical) {
+                        // apply padding to links (if padding is a decimal apply floor to left and ceil to right)
+                        $(opts.targets).css({
+                            'padding-top': Math.floor(padding / 2), 'padding-bottom': Math.floor(padding / 2)
+                        });
+                    } else {
+                        $(opts.targets).css({
+                            'padding-left': Math.floor(padding / 2), 'padding-right': Math.floor(padding / 2)
+                        });
+                    }
+
+                }
+
+            });
+
+        },
+        destroy: function (opts) {
+
+            // Repeat over each element in selector
+            return this.each(function () {
+
+                // find unpadded size of links and reset link size to whole numbers
+                if (opts.vertical) {
+
+                    $(opts.targets).each(function () {
+                        // remove dynamic element padding
+                        $(this).css({ 'padding-top': '', 'padding-bottom': '', width: '' });
+                    });
+
+                } else {
+
+                    $(opts.targets).each(function () {
+                        // remove dynamic element padding
+                        $(this).css({ 'padding-left': '', 'padding-right': '', width: '' });
+                    });
+
+                }
+                
+                $(parent).removeClass('dynPadding_parent').removeData('dynPadding');
+
+            });
+
+        }
+    };
 	
-	$.fn.dynPadding = function(opts) {
-		
-		var defaults = {
-			targets: '#nav a',
-			remainder: '',
-			vertical: false
-		};
-		var opts = $.extend(defaults, opts);
-		
-		return this.each(function() {
+	$.fn.dynPadding = function() {
+
+	    var method = arguments[0];
+
+	    if (methods[method]) {
+
+	        method = methods[method];
+
+	        // Our method was sent as an argument, remove it using slice because it's not an argument for our method
+	        arguments = Array.prototype.slice.call(arguments, 1);
+
+	    } else if (typeof (method) == 'object' || !method) {
+
+	        method = methods.init;
+
+	    } else {
+
+	        $.error('Method ' + method + ' does not exist on jQuery.pluginName');
+	        return this;
+
+	    }
+
+	    // Use apply to sent arguments when calling our selected method
+	    return method.apply(this, arguments);
 			
-			var parent = $(this);
-			var link_size = 0;
-			var unpadded = 0;
-			
-			if (opts.vertical) {
-				var parent_size = parent.height();
-			} else {
-				var parent_size = parent.width();
-			}
-			
-			var link_count = $(opts.targets).length;
-			
-			// find unpadded size of links and reset link size to whole numbers
-			if (opts.vertical) {
-				$(opts.targets).each(function() {
-					// unpad element
-					$(this).css({ 'padding-top': '0', 'padding-bottom': '0' });
-					
-					// measure element size with borders
-					unpadded += $(this).outerHeight();
-					
-					// set element size to erase decimal sizing
-					link_size = $(this).height();
-					$(this).css({ height: link_size + 'px' });
-				});
-			} else {
-				$(opts.targets).each(function() {
-					$(this).css({ 'padding-left': '0', 'padding-right': '0' });
-					unpadded += $(this).outerWidth(true);
-					
-					if ( $.browser.mozilla ) { 
-						link_size = $(this).width()+ 1;
-						$(this).css({ width: link_size + 'px' });
-					} else {
-						link_size = $(this).width();
-						$(this).css({ width: link_size + 'px' });
-					}
-				});
-			}
-			
-			// find raw padding and convert to whole number padding
-			var raw_padding = (parent_size - unpadded)/link_count;
-			var padding = Math.floor(raw_padding);
-			
-			if (padding >= 34) { padding = 32; }
-		
-			if (opts.remainder.length > 0) {
-				// calculate remainder from decimal padding
-				remainder = Math.round((raw_padding - padding) * link_count);
-				
-				if (remainder >= 17) { remainder = 16; }
-				
-				if (opts.vertical) {
-					// apply padding to links (if padding is a decimal apply floor to left and ceil to right)
-					$(opts.targets).css({ 'padding-top': Math.floor(padding/2), 'padding-bottom': Math.floor(padding/2) });
-					// apply remainder
-					$(opts.remainder).css({ 'padding-top': Math.floor(remainder/2) + Math.floor(padding/2), 'padding-bottom': Math.floor(remainder/2) + Math.floor(padding/2) });
-				} else {
-					$(opts.targets).css({ 'padding-left': Math.floor(padding/2), 'padding-right': Math.floor(padding/2) });
-					$(opts.remainder).css({ 'padding-left': Math.floor(remainder/2) + Math.floor(padding/2), 'padding-right': Math.floor(remainder/2) + Math.floor(padding/2) });
-				}
-			} else {  
-				if (opts.vertical) {
-					// apply padding to links (if padding is a decimal apply floor to left and ceil to right)
-					$(opts.targets).css({ 'padding-top': Math.floor(padding/2), 'padding-bottom': Math.floor(padding/2) });
-				} else {
-					$(opts.targets).css({ 'padding-left': Math.floor(padding/2), 'padding-right': Math.floor(padding/2) });
-				}
-			}			
-					
-		});
-			
-	}; /* END Dynamic Padding function */
+	}; 
+    /* END Dynamic Padding function */
 	
 })(jQuery);
